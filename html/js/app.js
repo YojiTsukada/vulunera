4d5bc3764ce6476fb09a5f58245fca0e6a432ed4
Yoji Tsukada <ytsukada@ytsukada-mbp.local>
2018-11-09 20:39:41 +0900

    trancate filterを追加

--- html/js/app.js
+++ html/js/app.js
@@ -9,5 +9,24 @@ var vm = new Vue({
             var getURL = "sample/sample.json"
             axios.get(getURL)
                 .then(response => (this.info = response.data))
+        },
+    filters: {
+        title_tranc: function (value) {
+            len = 10
+            end = "..."
+            if (value.length > len){
+                return value.substring(0, len) + end;
+            }
+            return value
+        },
+        text_tranc: function (value) {
+            len = 100
+            end = "..."
+            if (value.length > len) {
+                return value.substring(0, len) + end;
+            }
+            return value
         }
-    })
+    }
+})
+
